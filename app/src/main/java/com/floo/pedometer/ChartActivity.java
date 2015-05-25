33bbece1a2cdb0544c65278a17b8a771f955b26f
haqiqi <haqiqi26@gmail.com>
2015-05-25 20:08:19 +0700

    Rejected commit: Default

---
+++ app/src/main/java/com/floo/pedometer/ChartActivity.java
@@ -0,0 +1,106 @@
+package com.floo.pedometer;
+
+import android.animation.ObjectAnimator;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+
+public class ChartActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chart);
+       // setContentView(R.layout.graph_image);
+
+        String days[] = {"M","T","W","Th","F","S","Su"};
+        ArrayList<ProgressBar> pb = new ArrayList<>();
+
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.relative);
+
+        Random rand = new Random();
+
+        for(int i=0;i<10;i++) {
+
+            TextView tv = new TextView(ChartActivity.this);
+            TextView day = new TextView(ChartActivity.this);
+
+            ProgressBar progressBar = new ProgressBar(ChartActivity.this, null, android.R.attr.progressBarStyleHorizontal);
+            progressBar.setIndeterminate(false);
+            progressBar.setMax(500);
+            progressBar.setProgress(0);
+            progressBar.setProgressDrawable(getResources().getDrawable(R.drawable.custom_progress));
+
+            progressBar.setVisibility(View.VISIBLE);
+            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,50);
+            params.setMargins(0, 70 * i, 0, 0);
+
+            int x = rand.nextInt(400);
+            tv.setText(x + " M");
+            tv.setTextSize(15);
+            tv.setGravity(Gravity.CENTER);
+
+            day.setText(days[x % 7]);
+            day.setTextSize(15);
+            day.setGravity(Gravity.CENTER);
+            day.setBackgroundResource(R.color.light_green);
+
+            RelativeLayout.LayoutParams params2 = new RelativeLayout.LayoutParams(50,50);
+            params2.setMargins(0, 70 * i, 0, 0);
+
+
+            layout.addView(progressBar, params);
+            layout.addView(tv, params);
+
+            layout.addView(day, params2);
+
+            pb.add(progressBar);
+
+            ObjectAnimator animation = ObjectAnimator.ofInt(progressBar, "progress", 1, x);
+            animation.setDuration(2000); //in milliseconds
+            //animation.setInterpolator(new DecelerateInterpolator());
+            animation.start();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_chart, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        this.finish();
+        overridePendingTransition(
+                getIntent().getIntExtra("anim id in", R.anim.left_in),
+                getIntent().getIntExtra("anim id out", R.anim.left_out));
+    }
+}
---
+++ app/src/main/java/com/floo/pedometer/WebviewKit.java
@@ -0,0 +1,110 @@
+package com.floo.pedometer;
+
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+
+
+
+public class WebviewKit extends ActionBarActivity {
+
+    ProgressBar progressBar;
+    ImageButton mailButton,centerButton,userButton;
+    MediaPlayer mp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview_kit);
+        //setContentView(R.layout.home_image);
+
+        progressBar = (ProgressBar) findViewById(R.id.myProgress);
+        mailButton = (ImageButton) findViewById(R.id.mailButton);
+        centerButton= (ImageButton) findViewById(R.id.centerButton);
+        userButton = (ImageButton) findViewById(R.id.userButton);
+
+        progressBar.setRotation(90);
+
+        ObjectAnimator animation = ObjectAnimator.ofInt(progressBar, "progress", 1, 350);
+        animation.setDuration(2000); //in milliseconds
+        animation.setInterpolator(new DecelerateInterpolator());
+        animation.start();
+
+
+        userButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new SoundPlayer().execute();
+
+            }
+        });
+
+        mailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new SoundPlayer().execute();
+            }
+        });
+        centerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new SoundPlayer().execute();
+
+                Intent i = new Intent(WebviewKit.this,ChartActivity.class);
+                i.putExtra("anim id in", R.anim.up_in);
+                i.putExtra("anim id out", R.anim.up_out);
+                startActivity(i);
+                overridePendingTransition(R.anim.down_in, R.anim.down_out);
+            }
+        });
+    }
+
+    private class SoundPlayer extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            mp = MediaPlayer.create(WebviewKit.this, Uri.parse("/system/media/audio/ui/Effect_Tick.ogg"));
+            mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    // TODO Auto-generated method stub
+                    mp.release();
+                }
+
+            });
+            mp.start();
+            return null;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_webview_kit, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
